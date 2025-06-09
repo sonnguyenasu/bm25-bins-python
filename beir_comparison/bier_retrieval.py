7507e1a192781d1ba756598c86468c6a570b21f9
dkblackley <dblackle@gmu.edu>
2025-06-09 16:37:33 -0400

    BM25 not working.

--- beir_comparison/bier_retrieval.py
+++ beir_comparison/bier_retrieval.py
@@ -248,7 +248,133 @@ class ngramBM25Retriever(BaseSearch):
             for token in tokens:
                 results = new_lookup.get(token)
 
-                if results is None: # this word does't appear in our corpus
+                if results is None: # this word doesn't appear in our corpus
+                    logging.warning(f"missing word: {qid}, {token}")
+                    continue
+
+                for doc_id, score in results.items():
+                    doc_items = original_corpus.get(doc_id)
+                    new_corpus[doc_id] = doc_items
+
+        final_bm25 = BM25Search(
+            index_name=self.index_name,
+            hostname=self.hostname,
+            initialize=self.initialize,
+            number_of_shards=self.shards,
+            retry_on_timeout=True,
+            timeout=600
+        )
+        results = final_bm25.search(new_corpus, original_queries, top_k, score_function)
+        return results
+
+
+class ngramBM25Retriever_freq(BaseSearch):
+    def __init__(self,
+                 hostname: str = "http://fedora-ripley.tail0c8c1f.ts.net:9200",
+                 index_name: str = "msmarco",
+                 initialize: bool = True,
+                 n: int = 2,
+                 shards: int = 1,
+                 k = 10,
+                 frequency = 10,):
+        super().__init__()
+
+        self.index_name = index_name
+        self.hostname = hostname
+        self.initialize = initialize
+        self.shards = shards
+        self.k = k
+
+        logging.info(f"[TokenizedBM25] ES host={hostname}, index={index_name}")
+        self.n = n
+        self.frequency = frequency
+
+    def search(self,
+               corpus: dict[str, dict[str, str]],
+               queries: dict[str, str],
+               top_k: int,
+               score_function,
+               **kwargs
+               ) -> dict[str, dict[str, float]]:
+
+        original_queries = deepcopy(queries)
+        original_queries = {qid: " ".join(tokenize(q)) for qid, q in original_queries.items()}
+        original_corpus = deepcopy(corpus)
+
+        # corpus is a str of DOC IDs mapping to a dict of 'text' and 'title'
+        # queries are QUERY IDs mapping to the query
+
+        # result is supposed to be QID mapping to DOC ID and its BM25 score
+
+        print("Beginning ngram BM25")
+
+        keywords = set()
+
+        for item in corpus.values():
+            keywords.update(tokenize(item["title"]))
+            keywords.update(tokenize(item["text"]))
+
+        # to ensure that no score drags any others down, we do an unigram analysis
+
+        queries = {}
+        for word in keywords:
+            queries[f"{word}"] = f"{word}"
+
+        unigram_bm25 = BM25Search(
+            index_name=self.index_name,
+            hostname=self.hostname,
+            initialize=self.initialize,
+            number_of_shards=self.shards,
+            retry_on_timeout=True,
+            timeout=600
+        )
+
+        # We hope that this will produce at least 1 doc per keyword
+        unigram_hits = unigram_bm25.search(corpus, queries, len(corpus.keys()), score_function)
+
+
+        unigram_scores = {}
+        # A dictionary mapping docID to keywords that appear and their score
+        docs_with_scores: dict[str, dict[str, float]] = {}
+
+
+        for key, val in unigram_hits.items():
+
+            for doc_id in val.keys():
+                if doc_id not in docs_with_scores:
+                    docs_with_scores[doc_id] = {}
+                docs_with_scores[doc_id][key] = val[doc_id]
+
+            score = sum(val.values())
+            logging.debug(f"{key}, {score}")
+            unigram_scores[key] = score
+
+
+        # Mapping from doc id to the list of self.frequency number of ngrams
+        requested_words: dict[str, list[str]] = {}
+
+        for doc_id, score in docs_with_scores.items():
+            top_keywords = sorted(score.items(), key=lambda x: x[1], reverse=True)[:self.frequency]
+        
+
+
+
+
+
+        new_lookup = {}
+        new_corpus = {}
+        for key, hits in final_hits.items():  # ngramlookup has some dead values
+            for word in tokenize(ngram_lookup[key]):
+                new_lookup[word] = hits
+
+        for qid, query_text in original_queries.items():
+            tokens = tokenize(query_text)
+
+            for token in tokens:
+                results = new_lookup.get(token)
+
+                if results is None: # this word doesn't appear in our corpus
+                    logging.warning(f"missing word: {qid}, {token}")
                     continue
 
                 for doc_id, score in results.items():
@@ -277,8 +403,10 @@ def main():
     )
 
     dataset = "quora"
+    dataset = "nfcorpus"
     url = f"https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{dataset}.zip"
-    out_dir = os.path.join(pathlib.Path(__file__).parent, "datasets")
+    # out_dir = os.path.join(pathlib.Path(__file__).parent, "datasets")
+    out_dir = "/home/yelnat/Documents/Nextcloud/10TB-STHDD/Sync-Folder-STHDD/datasets"
     data_path = util.download_and_unzip(url, out_dir)
 
     corpus, queries, qrels = GenericDataLoader(data_path).load(split="test")
@@ -287,10 +415,10 @@ def main():
 
         print("======================= RESULTS FOR n = {i} =======================".format(i=i))
 
-        model = ngramBM25Retriever(n=i)
+        model = ngramBM25Retriever_freq(n=i)
         # model = RegularBM25()
 
-        retriever = EvaluateRetrieval(model, k_values=[10, 100])
+        retriever = EvaluateRetrieval(model, k_values=[1000])
         results = retriever.retrieve(corpus, queries)
 
         logging.info(f"Evaluation for k in {retriever.k_values}")
